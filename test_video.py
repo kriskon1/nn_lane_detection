68e2590da5537f65c9d078a5e011938fda51baea
kriskon1 <126876298+kriskon1@users.noreply.github.com>
2025-02-06 16:18:59 +0100

    Add files via upload

---
+++ test_video.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+from ultralytics import YOLO
+import utils
+from skimage.morphology import skeletonize
+
+
+WIDTH = 1280
+HEIGHT = 720
+INPUT_WIDTH = 256
+INPUT_HEIGHT = 256
+TARGET_CLASSES = [2, 7]
+
+coco_class_list = open("coco.txt", "r")
+data = coco_class_list.read()
+class_list = data.split("\n")
+
+
+# model = load_model('model/model_for_lane_tuned_adam_binary_batch16_v2.keras')
+# model = load_model('model/model_for_lane_tuned_adam_binary_batch16_resnet50_unet_fixed.keras')
+# model = load_model('model/model_unet_aug_att.keras')
+model = load_model('model/model_unet_att.keras')
+# model = load_model('model/model_unet_resnet50_unet_att_conv2d.keras')
+# model = load_model('model/model_unet_resnet50_unet_aug_att_conv2d.keras')
+model_YOLO = YOLO("model/yolov10n.pt")
+
+
+# cap = cv2.VideoCapture("D:/Programozás/Python/Lane_detection/videos/video.hevc")
+# cap = cv2.VideoCapture("D:/Programozás/Python/Lane_detection/videos/framestestvideo24.avi")
+cap = cv2.VideoCapture("D:/Programozás/Python/Lane_detection/videos/tusimple_video.avi")
+# cap = cv2.VideoCapture("D:/Programozás/Python/Lane_detection/videos/NO20230924-121041-000402F.mp4")
+# cap = cv2.VideoCapture("D:/Programozás/Python/Lane_detection/videos/IMG_8006.mov")
+# cap = cv2.VideoCapture("D:/Programozás/Python/Lane_detection/videos/IMG_8007.MOV")
+# cap = cv2.VideoCapture("D:/Programozás/Python/Lane_detection/videos/IMG_8008.mov")
+
+
+while cap.isOpened():
+    success, frame_orig = cap.read()
+    if success:
+        frame_orig = cv2.resize(frame_orig, (WIDTH, HEIGHT), interpolation=cv2.INTER_LINEAR)
+        print(f"Frame original shape: {frame_orig.shape}")
+        frame_orig_blurred = cv2.GaussianBlur(frame_orig, (5, 5), 0)
+
+        frame = cv2.resize(frame_orig_blurred, (INPUT_WIDTH, INPUT_HEIGHT))
+        print(f"Frame resized shape: {frame.shape}")
+        frame = np.expand_dims(frame, axis=0)
+        frame = frame / 255.0
+        print(f"Frame expanded shape: {frame.shape}")
+        lane_results = model.predict(frame)
+        print(f"Results original shape: {lane_results.shape}")
+        lane_results = np.squeeze(lane_results, axis=0)
+        print(f"Results squeezed shape: {lane_results.shape}")
+
+        vehicle_results = model_YOLO.track(frame_orig, persist=True)
+
+        frame_with_lanes = utils.draw_ego_lane(frame_orig, lane_results, WIDTH, HEIGHT)
+
+        cx, cy, x1, bly, x2, y2, img = utils.draw_vehicles(frame_with_lanes, vehicle_results[0], class_list, TARGET_CLASSES)
+        cv2.imshow("pred", img)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
---
+++ utils.py
@@ -0,0 +1,134 @@
+import cv2
+import numpy as np
+from skimage.morphology import skeletonize
+
+
+def clahe(img):
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    r, g, b = cv2.split(img)
+
+    output1_r = clahe.apply(r)
+    output1_g = clahe.apply(g)
+    output1_b = clahe.apply(b)
+    frame_orig_clahe = cv2.merge((output1_r, output1_g, output1_b))
+
+    return frame_orig_clahe
+
+
+def draw_lanes(img, prediction, width, height):
+    prediction = cv2.resize(prediction, (width, height), interpolation=cv2.INTER_LANCZOS4)
+    _, binary_mask = cv2.threshold(prediction, 0.5, 1.0, cv2.THRESH_BINARY)
+    skeleton = skeletonize(binary_mask)
+    skeleton = (skeleton * 255).astype(np.uint8)
+    roi = skeleton[350:, :]
+    skeleton_contours, _ = cv2.findContours(roi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in skeleton_contours:
+        contour[:, :, 1] += 350
+        if (cv2.arcLength(contour, True)) > 400:
+            cv2.polylines(img, [contour], isClosed=False, color=(0, 255, 0), thickness=2)
+    return img
+
+
+def draw_lane(img, prediction, width, height):     # alap rajzolás
+    prediction = cv2.resize(prediction, (width, height), interpolation=cv2.INTER_LANCZOS4)
+    mask = (prediction > 0.5).astype(np.uint8)
+    mask_resize = cv2.resize(mask, ((img.shape[1]), (img.shape[0])), interpolation=cv2.INTER_CUBIC)
+    img[mask_resize == 1, :] = (255, 0, 0)
+    return img
+
+
+def draw_ego_lane(img, prediction, width, height):
+    prediction = cv2.resize(prediction, (width, height), interpolation=cv2.INTER_LANCZOS4)
+    _, binary_mask = cv2.threshold(prediction, 0.5, 1.0, cv2.THRESH_BINARY)
+    skeleton = skeletonize(binary_mask)
+    skeleton = (skeleton * 255).astype(np.uint8)
+    roi = skeleton[350:, :]
+    contours, _ = cv2.findContours(roi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if contours:
+        image_center_x = prediction.shape[1] // 2
+
+        # Initialize variables to find the ego lane
+        left_ego_lane = None
+        right_ego_lane = None
+        min_left_distance = float('inf')
+        min_right_distance = float('inf')
+
+        for contour in contours:
+            if (cv2.arcLength(contour, True)) > 400:
+                x, y, w, h = cv2.boundingRect(contour)
+                lane_center_x = x + w // 2
+
+                # Classify contour as either left or right of the center
+                if lane_center_x < image_center_x:
+                    # Check if it's the closest to the center on the left side
+                    distance_to_center = abs(image_center_x - lane_center_x)
+                    if distance_to_center < min_left_distance:
+                        min_left_distance = distance_to_center
+                        left_ego_lane = contour
+                elif lane_center_x > image_center_x:
+                    # Check if it's the closest to the center on the right side
+                    distance_to_center = abs(lane_center_x - image_center_x)
+                    if distance_to_center < min_right_distance:
+                        min_right_distance = distance_to_center
+                        right_ego_lane = contour
+
+        # Draw the identified left and right ego lane boundaries
+        if left_ego_lane is not None:
+            left_ego_lane[:, :, 1] += 350
+            cv2.drawContours(img, [left_ego_lane], -1, (255, 0, 0), 4)
+
+        if right_ego_lane is not None:
+            right_ego_lane[:, :, 1] += 350
+            cv2.drawContours(img, [right_ego_lane], -1, (0, 0, 255), 4)
+
+    return img
+
+
+def draw_vehicles(img, results, class_list, target_classes):
+    filtered_detections = []
+    cx, cy, x1, bly, x2, y2 = None, None, None, None, None, None
+    if len(results.boxes) > 0:
+        for detection in results.boxes:
+            class_id = int(detection.cls)
+            if class_id in target_classes:
+                filtered_detections.append(detection)
+
+        for det in filtered_detections:
+            x1, y1, x2, y2 = map(int, det.xyxy[0])
+            _, _, w, h = map(int, det.xywh[0])
+            conf = det.conf[0]
+            cx = x1 + w // 2
+            cy = y1 + h // 2
+            bly = y1+h          # box bottom left y coord
+            class_id = class_list[int(det.cls)]
+            track_id = int(det.id) if det.id is not None else ""
+
+            # Draw bounding box and track ID
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
+            # cv2.circle(img, (cx, cy), 6, (0, 255, 255), -1)
+            # cv2.circle(img, (x1, y1), 6, (255, 0, 0), -1)
+            # cv2.circle(img, (x1, bly), 6, (0, 255, 0), -1)    # box bottom left coord
+            # cv2.circle(img, (x2, y2), 6, (0, 0, 255), -1)      # box bottom right coord
+            cv2.putText(img, f'ID: {track_id}, {class_id}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 1)
+
+        return cx, cy, x1, bly, x2, y2, img
+
+    else:
+        return cx, cy, x1, bly, x2, y2, img
+
+
+def ft(prediction, width, height):
+    prediction = cv2.resize(prediction, (width, height), interpolation=cv2.INTER_LANCZOS4)
+
+    dft = np.fft.fft2(prediction, axes=(0, 1))
+    dft_shift = np.fft.fftshift(dft)
+    mask = np.zeros_like(prediction)
+    mask2 = cv2.GaussianBlur(mask, (31, 31), 0)
+    mask2 = cv2.GaussianBlur(mask2, (31, 31), 0)
+    mask2 = cv2.GaussianBlur(mask2, (31, 31), 0)
+    dft_shift_masked2 = np.multiply(dft_shift, mask) / 255
+    back_ishift_masked2 = np.fft.ifftshift(dft_shift_masked2)
+    img_filtered2 = np.fft.ifft2(back_ishift_masked2, axes=(0, 1))
+    img_filtered2_final = np.abs(img_filtered2).clip(0, 255).astype(np.uint8)
+
+    return img_filtered2_final
